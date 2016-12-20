f843de67ab405ecd3865d08fe707927e4f9158f3
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-12-20 22:04:31 +0800

    Add simple step to save player name

--- game/game_play.py
+++ game/game_play.py
@@ -1,7 +1,11 @@
 class GamePlay(object):
     def __init__(self):
         self.state = 'NEW'
+        self.players = []
 
     def prompt(self):
-        return 'Enter name for Player 1:\n>> '
+        yield 'Enter name for Player 1:\n>> '
+
+    def next_step(self, resp):
+        self.players.append(resp)
 
--- tests/test_game_play.py
+++ tests/test_game_play.py
@@ -4,4 +4,12 @@ from .context import GamePlay
 class TestGamePlay:
     def test_prompt(self):
         g = GamePlay()
-        assert g.prompt() == 'Enter name for Player 1:\n>> '
+        first_prompt = list(g.prompt())[0]
+        assert first_prompt == 'Enter name for Player 1:\n>> '
+
+    def test_save_player(self):
+        g = GamePlay()
+        new_player = 'Player X'
+        g.next_step(new_player)
+        assert len(g.players) == 1
+        assert g.players[0] == new_player
