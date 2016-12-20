26ad2a58d25ab292cc8a1960809616c56da1fa1b
Chua Jie Sheng <chuajiesheng@gmail.com>
2016-12-20 22:53:42 +0800

    Remove test class

--- tests/test_game_play.py
+++ tests/test_game_play.py
@@ -1,15 +1,15 @@
 from .context import GamePlay
 
 
-class TestGamePlay:
-    def test_prompt(self):
-        g = GamePlay()
-        first_prompt = list(g.prompt())[0]
-        assert first_prompt == 'Enter name for Player 1:\n>> '
+def test_prompt():
+    g = GamePlay()
+    first_prompt = list(g.prompt())[0]
+    assert first_prompt == 'Enter name for Player 1:\n>> '
 
-    def test_save_player(self):
-        g = GamePlay()
-        new_player = 'Player X'
-        g.next_step(new_player)
-        assert len(g.players) == 1
-        assert g.players[0] == new_player
+
+def test_save_player():
+    g = GamePlay()
+    new_player = 'Player X'
+    g.next_step(new_player)
+    assert len(g.players) == 1
+    assert g.players[0] == new_player
